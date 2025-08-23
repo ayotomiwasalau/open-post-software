f4b36c909fe31b89b65045e2a46b9f9648a72f0e
ayotomiwasalau <ayotomiwasalau@gmail.com>
2025-08-23 15:25:24 +0100

    update terrafrom ci

--- Iac/terraform/version.tf
+++ Iac/terraform/version.tf
@@ -3,6 +3,8 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+
+
 terraform {
   required_version = "1.12.2"
   required_providers {
@@ -10,5 +12,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.55.0"
     }
+  }
+  
+  backend "s3" {
+    bucket = "s3://deploy-artifacts-aytom/terraform"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
   }
 }
